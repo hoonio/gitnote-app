004a3db6468a2c5c25a8d7483a22b5fa3e8bf35d
Hoonio <hoonio@hoonio.com>
2016-09-21 00:52:36 +0100

    leverage auth base encoding with objective-c code

--- AppContainer.js
+++ AppContainer.js
@@ -13,7 +13,7 @@ export default class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedTab: 'search'
+      selectedTab: 'feed'
     }
   }
   render() {
--- AuthService.js
+++ AuthService.js
@@ -1,6 +1,8 @@
-var buffer = require('buffer');
+// var buffer = require('buffer');
 var AsyncStorage = require('react-native').AsyncStorage;
 var _ = require('lodash');
+var encoding = require('NativeModules').Encoding;
+
 
 const authKey = 'auth';
 const userKey = 'user';
@@ -34,43 +36,42 @@ class AuthService {
   }
 
   login(creds, cb) {
-    let b = new buffer.Buffer(creds.username +
-      ':' + creds.password);
-    let encodedAuth = b.toString('base64');
-
-    fetch('https://api.github.com/user', {
-        headers: {
-          'Authorization': 'Basic ' + encodedAuth
-        }
-      })
-      .then((response) => {
-        if (response.status >= 200 && response.status < 300) {
-          return response;
-        }
-        throw {
-          badCredentials: response.status == 401,
-          unknownError: response.status != 401
-        }
-      })
-      .then((response) => {
-        return response.json();
-      })
-      .then((results) => {
-        AsyncStorage.multiSet([
-          [authKey, encodedAuth],
-          [userKey, JSON.stringify(results)]
-        ], (err) => {
-          if (err) {
-            throw err;
+    const authStr = creds.username + ':' + creds.password;
+    encoding.base64Encode(authStr, (encodedAuth)=>{
+      fetch('https://api.github.com/user', {
+          headers: {
+            'Authorization': 'Basic ' + encodedAuth
           }
-          return cb({
-            success: true
-          });
         })
-      })
-      .catch((err) => {
-        return cb(err);
-      });
+        .then((response) => {
+          if (response.status >= 200 && response.status < 300) {
+            return response;
+          }
+          throw {
+            badCredentials: response.status == 401,
+            unknownError: response.status != 401
+          }
+        })
+        .then((response) => {
+          return response.json();
+        })
+        .then((results) => {
+          AsyncStorage.multiSet([
+            [authKey, encodedAuth],
+            [userKey, JSON.stringify(results)]
+          ], (err) => {
+            if (err) {
+              throw err;
+            }
+            return cb({
+              success: true
+            });
+          })
+        })
+        .catch((err) => {
+          return cb(err);
+        });
+    });
   }
 }
 
